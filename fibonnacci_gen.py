bfe8148f869c8358e8d1b8b48ae27b520413387f
soujanya gangula <soujanya.gangula@infor.com>
2020-07-28 18:01:34 +0530

    added generators

---
+++ fibonnacci_gen.py
@@ -0,0 +1,22 @@
+"""fibonnacci series using generators"""
+def fib_gen(n):
+    a,b=0,1
+    for i in range(n):
+        yield a
+        a,b=b,a+b
+
+#genobj=fib_gen(0)
+#for i in genobj:
+    #print(i)
+
+"""fibonnacci series without generators"""
+def fib(n):
+    a,b = 0,1
+    print(a)
+    print(b)
+    for i in range(2,n):
+        c = a+b
+        a = b
+        b = c
+        print(c)
+print(fib(5))
